967931bac7f8290fcf9c96d435dbeadde2675770
RawabyRahal <rawaby.16.4@gmail.com>
2023-12-11 16:50:56 +0200

    Updating Renderer

--- Renderer.js
+++ Renderer.js
@@ -1,55 +1,29 @@
-const sourceUser = $('#user-template').html();
-const templateUser = Handlebars.compile(sourceUser);
-
-const sourceFriends = $('#freinds-template').html();
-const templateFriends = Handlebars.compile(sourceFriends);
-
-const sourceAboutme = $('#aboutme-template').html();
-const templateAboutme = Handlebars.compile(sourceAboutme);
-
-const sourceQuote = $('#quote-template').html();
-const templateQuote = Handlebars.compile(sourceQuote);
-
-const sourcePokemons = $('#pokemon-template').html();
-const templatePokemons = Handlebars.compile(sourcePokemons);
-
-
-class Renderer {
-    constructor() {
-
+class Component {
+    constructor(templateSelector, containerSelector) {
+        this.template = Handlebars.compile($(templateSelector).html())
+        this.container = $(containerSelector)
     }
-
-    renderData = function (data) {
-        
-        console.log(data.user)
-        $('.user-container').empty()
-        $('.friends-container').empty()
-        $('.meat-container').empty()
-        $('.quote-container').empty()
-        $('.pokemon-container').empty()
-
-        //user
-        const newHTMLUser = templateUser(data);
-        $('.user-container').append(newHTMLUser);
-
-        // friends
-        const newHTMLFriends = templateFriends(data);
-        $('.friends-container').append(newHTMLFriends);
-
-        // aboutMe
-        const newHTMLAboutMe = templateAboutme(data);
-        $('.meat-container').append(newHTMLAboutMe);
-
-        // quote
-        const newHTMLQuote = templateQuote(data);
-        $('.quote-container').append(newHTMLQuote);
-
-        // Pokemon
-        data.pokemon.name = data.pokemon.name.charAt(0).toUpperCase() + data.pokemon.name.slice(1)
-        console.log(data.pokemon.name)
-        const newHTMLPokemon = templatePokemons(data);
-        // console.log(data.pokemon.name.charAt(0).toUpperCase() + data.pokemon.name.slice(1))
-        $('.pokemon-container').append(newHTMLPokemon);
+    display(data) {
+        this.container.empty()
+        this.container.append(this.template(data));
     }
 }
 
+class Renderer {
+    constructor() {
+        this.userCompenent = new Component('#user-template', '.user-container')
+        this.friendsCompenent = new Component('#freinds-template', '.friends-container')
+        this.aboutmeCompenent = new Component('#aboutme-template', '.meat-container')
+        this.quoteCompenent = new Component('#quote-template', '.quote-container')
+        this.pokemonsCompenent = new Component('#pokemon-template', '.pokemon-container')
+    }
+    
+    renderData = function (data) {
+        data.pokemon.name = data.pokemon.name.charAt(0).toUpperCase() + data.pokemon.name.slice(1)
+        this.userCompenent.display(data)
+        this.friendsCompenent.display(data)
+        this.aboutmeCompenent.display(data)
+        this.quoteCompenent.display(data)
+        this.pokemonsCompenent.display(data)
+    }
+}
